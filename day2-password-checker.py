1bbafaabd8ba3e83108bd08310c37c3002f4289b
Vishnupriya Golla <gollavishnupriya1@gmail.com>
2026-05-14 20:02:42 +0530

    Added password checker project

---
+++ day2-password-checker.py
@@ -0,0 +1,7 @@
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+password_length = len(password)
+hidden_password = "*" * password_length
+
+print(f"{username}, your password {hidden_password} is {password_length} characters long.")
